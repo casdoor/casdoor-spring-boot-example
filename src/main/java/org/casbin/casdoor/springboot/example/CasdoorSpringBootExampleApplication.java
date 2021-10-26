bb8c4e493dcfe43a2b6b19d3dc0fe5df22dda0f7
“seriouszyx” <674965440@qq.com>
2021-10-26 21:20:28 +0800

    feat: init springboot project
    Signed-off-by: “seriouszyx” <674965440@qq.com>

---
+++ src/main/java/org/casbin/casdoor/springboot/example/CasdoorSpringBootExampleApplication.java
@@ -0,0 +1,16 @@
+package org.casbin.casdoor.springboot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yixiang Zhao (@seriouszyx)
+ */
+@SpringBootApplication
+public class CasdoorSpringBootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasdoorSpringBootExampleApplication.class, args);
+    }
+
+}
